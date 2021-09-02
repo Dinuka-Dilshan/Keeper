7a72bccb648f76c419020f0ec39a52f51f069055
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-09-02 13:33:00 +0530

    note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Note = ()=>{
+    return(
+        <div className="border-1 w-auto px-1 m-1 pb-1 pt-1 mt-1">
+            <h4 className=" font-normal fw-bold mt-1 pt-1">This is a note title</h4>
+            <p className="font-normal fw-700 my-1">This is a note content</p>
+        </div>
+    );
+}
+
+
+export default Note;
