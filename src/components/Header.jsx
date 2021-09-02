6bb4ea1193215897bfc501c5ad0cefb0f3f2d5a3
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-09-02 12:20:16 +0530

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = ()=>{
+
+    return(
+
+        <div className="d-flex align-items-center p-1 fs-1 bg-primary text-white">
+                <span>Keeper</span>
+        </div>
+
+    );
+
+}
+
+
+export default Header;
