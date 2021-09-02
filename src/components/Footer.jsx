b15633218864246447a55ca44d9a36a72aa45010
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-09-02 13:43:46 +0530

    change app.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const year = new Date().getFullYear();
 
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <h4 className="position-absolute text-center bottom-0 w-100 m-0 text-gray pb-1 font-normal">
         Dinuka Dilshan &copy; {year}
